4cf40c9d17b777a3e362e43536462d9e3dc6875a
disanda <skyanda1@qq.com>
2021-03-07 18:58:28 +0800

    x

---
+++ data.py
@@ -0,0 +1,113 @@
+# coding=utf-8
+import os
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+
+class DatasetFromFolder(Dataset):
+    def __init__(self,path='',transform=None, channels = 3):
+        super().__init__()
+        self.channels = channels
+        self.path = path
+        self.transform = transform
+        self.image_filenames = [x for x in os.listdir(self.path) if x.endswith('jpg') or x.endswith('png')] # x.startswith() 
+        #imgs_path = os.listdir(path)
+        #self.image_filenames = list(filter(lambda x:x.endswith('jpg') or x.endswith('png') ,imgs_path))
+    def __getitem__(self, index):
+        if self.channels == 1:
+            a = Image.open(os.path.join(self.path, self.image_filenames[index])).convert('L') # 'L'是灰度图, 'RGB'彩色
+        elif self.channels ==3:
+            a = Image.open(os.path.join(self.path, self.image_filenames[index])).convert('RGB')
+        else:
+            print('error')
+        #a = a.resize((self.size, self.size), Image.BICUBIC)
+        #a = transforms.ToTensor()(a)
+        if self.transform:
+            a = self.transform(a)
+        return a
+    def __len__(self):
+        return len(self.image_filenames)
+
+def make_dataset(dataset_name, batch_size,img_size,drop_remainder=True, shuffle=True, num_workers=4, pin_memory=False,img_paths=''):
+    if dataset_name == 'mnist' or dataset_name=='fashion_mnist':
+        transform = transforms.Compose([
+            transforms.Resize(size=(img_size, img_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
+        if dataset_name == 'mnist':
+            dataset = datasets.MNIST('data/MNIST', transform=transform, download=True)
+        else:
+            dataset = datasets.FashionMNIST('data/FashionMNIST', transform=transform, download=True)
+        img_shape = [img_size, img_size, 1]
+    elif dataset_name == 'cifar10':
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            #transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+        dataset = datasets.CIFAR10('data/CIFAR10', transform=transform, download=True)
+        img_shape = [32, 32, 3]
+    elif dataset_name == 'pose10':
+        transform_pose10 = transforms.Compose([
+            transforms.Resize(size=(img_size, img_size)),
+            transforms.ToTensor(),
+            #transforms.Normalize(mean=[0.5], std=[0.5]) #黑白应该是不用norm
+        ])
+        #dataset = DatasetFromFolder(path='/_yucheng/dataSet/pose/pose_set_10',size=img_size)
+        #dataset = DatasetFromFolder(path='./data/Pose/pose_set_10',size=img_size)
+        path_pose10='./data/Pose/pose_set_10'
+        dataset = DatasetFromFolder(path=path_pose10,transform=transform_pose10,channels=1)
+        img_shape = [img_size, img_size, 1]
+    elif dataset_name == 'celeba_64':
+        crop_size = 108
+        offset_height = (218 - crop_size) // 2
+        offset_width = (178 - crop_size) // 2
+        crop = lambda x: x[:, offset_height:offset_height + crop_size, offset_width:offset_width + crop_size]# [image,height,width]
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Lambda(crop),
+            transforms.Resize(size=(img_size, img_size)),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            #transforms.ToPILImage()
+            ])
+        dataset = DatasetFromFolder(path='',size=64)
+        img_shape = (img_size, img_size, 3)
+    elif dataset_name == 'celeba_HQ':
+        transform_128 = transforms.Compose([
+            transforms.Resize(size=(img_size, img_size)),
+            transforms.ToTensor(),
+            #transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+            ])
+        #path_128 = 'F:/dataSet2/CelebAMask-HQ/CelebA-HQ-img'
+        #path_128 = '/home/disanda/Desktop/dataSet/CelebAMask-HQ/img-30000/CelebA-HQ-img/'
+        path_128 = '/_yucheng/dataSet/CelebAMask-HQ/CelebAMask-HQ/CelebA-HQ-img'
+        dataset = DatasetFromFolder(path=path_128,transform=transform_128,channels=3)
+        img_shape = (img_size, img_size, 3)
+    else:
+        raise NotImplementedError
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_remainder, pin_memory=pin_memory)
+    return data_loader, img_shape
+
+# ==============================================================================
+# =                                   debug                                    =
+# ==============================================================================
+
+# pose = DatasetFromFolder('/Users/apple/Desktop/AI_code/dataSet/pose_set_1000')
+
+# train_loader = DataLoader(
+#      dataset=pose,
+#      batch_size=25,#一个batch25张图片,epoch=allData_size/batch_size
+#      shuffle=False,
+#      #num_workers=0,若是win需要这一行
+#      pin_memory=True,#用Nvidia GPU时生效
+#      drop_last=True
+#  )
+
+# import tqdm
+# for x_real in tqdm.tqdm(train_loader, desc='Inner Epoch Loop'):
+#     print(type(x_real))
+
+# for i, x in enumerate(train_loader):
+#      print(i)
+#      print(x.shape)#[n,c,w,h]
+#      torchvision.utils.save_image(x, './pose-img/%d.jpg'%(i), nrow=5)#这个保存是三通道的,需要改成1通道
---
+++ networks/D2E_PG.py
@@ -0,0 +1,134 @@
+# ----------自己写的一套DCGAN网络，可以通过图像分辨率调整网络参数，包括输入维度，中间维度.--------
+
+# 1. 以及不同分辨率的对应不同上采样数(即网络的层数)，默认第一次上采样，像素从 1->4。 之后每一次上采样，像素增加一倍(这里应该是长宽都增加一倍).
+# 2. input_dim 对应G的《输入维度》，image_size表示《生成图片对应的像素》, first_hidden_dim对应《网络中间层维度》(中间层起点的维度)
+# 3. scale是input_dim放大的倍数，用于决定中间隐藏层起始时的size, 其和input_dim共同决定网络参数的规模.
+# 4. 如果希望G的输入维度 和 D的输出维度对称, 则 first_hidden_dim = input_dim * scale
+
+# 测试网络规模:
+# import networks.network_1 as net
+# G = net.Generator(input_dim=32, image_size=256, scale=32)
+# D = net.Discriminator_SpectrualNorm(input_dim=32, image_size=256, scale=16)
+# x,y = net.get_parameter_number(G),net.get_parameter_number(D)
+# x_G, y_G = net.get_para_GByte(G),net.get_para_GByte(D)
+
+#D2E , 在DCGAN下，比较E的参数和G的参数对性能的影响
+#D2E_PG , 在PGGAN(EQL和PixelNormLyer)下比较s
+
+import torch
+from torch import nn
+import torch.nn.utils.spectral_norm as spectral_norm
+import math
+import torch.nn.functional as F
+
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+def get_para_GByte(parameter_number):
+     x=parameter_number['Total']*8/1024/1024/1024
+     y=parameter_number['Total']*8/1024/1024/1024
+     return {'Total_GB': x, 'Trainable_BG': y}
+
+class PixelNormLayer(nn.Module):
+    def __init__(self, inchannel=0, epsilon=1e-8):
+        super().__init__()
+        self.epsilon = epsilon
+    def forward(self, x):
+        return x * torch.rsqrt(torch.mean(x**2, dim=1, keepdim=True) + self.epsilon)
+
+class EqualConv2d(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, bias=True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(out_channel, in_channel, kernel_size, kernel_size))
+        self.scale = 1 / math.sqrt(in_channel * kernel_size ** 2)
+        self.stride = stride
+        self.padding = padding
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_channel))
+        else:
+            self.bias = None
+    def forward(self, input):
+        out = F.conv2d(input,self.weight * self.scale,bias=self.bias,stride=self.stride,padding=self.padding)
+        return out
+
+class Generator(nn.Module):
+    def __init__(self, input_channels=512, output_channels=3, up_times=5): # up_times=5 就是256*256
+        super().__init__()
+        layers = []
+        bias_flag = False
+
+        # 1: 1x1 -> 4x4
+        layers.append(nn.ConvTranspose2d(512, 512, kernel_size=4,stride=1,padding=0,bias=bias_flag))
+        layers.append(PixelNormLayer(512))
+        layers.append(nn.ReLU())
+
+        layers.append(nn.ConvTranspose2d(512, 512, kernel_size=3,stride=1,padding=1,bias=bias_flag))
+        layers.append(PixelNormLayer(512))
+        layers.append(nn.ReLU())
+
+        # 2: upsamplings, 4x4 -> 8x8 -> 16x16 -> 32*32 -> 64 -> 128
+        while up_times>0:
+            layers.append(nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1, bias=bias_flag))
+            layers.append(PixelNormLayer(512))
+            layers.append(nn.ReLU())
+            layers.append(nn.ConvTranspose2d(512, 512, kernel_size=3, stride=1, padding=1, bias=bias_flag))
+            layers.append(PixelNormLayer(512))
+            layers.append(nn.ReLU())
+            up_times = up_times - 1
+
+
+        # 3:end 
+        layers.append(nn.ConvTranspose2d(512,256, kernel_size=4,stride=2, padding=1, bias=bias_flag))
+        PixelNormLayer()
+        layers.append(nn.Tanh())
+
+
+        layers.append(nn.ConvTranspose2d(256,output_channels,kernel_size=3, padding=1, bias=bias_flag))
+        PixelNormLayer()
+        layers.append(nn.Tanh())
+
+
+        # all
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, z):
+        x = self.net(z)
+        return x
+
+
+class Discriminator_SpectrualNorm(nn.Module):
+    def __init__(self, input_channels=512, output_channels=3, up_times=5, bias_flag = False):
+        super().__init__()
+        layers=[]
+
+        # 1:
+        layers.append(spectral_norm(EqualConv2d(3, 256, kernel_size=3, padding=1, bias=bias_flag)))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+        layers.append(spectral_norm(EqualConv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+        # 2: 64*64 > 4*4
+        while up_times>0:
+            layers.append(spectral_norm(EqualConv2d(512, 512, kernel_size=3, padding=1, bias=bias_flag)))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers.append(spectral_norm(EqualConv2d(512, 512, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            up_times = up_times - 1
+
+        # 3: 4*4 > 1*1
+        layers.append(spectral_norm(EqualConv2d(512, 512, kernel_size=3, padding=1, bias=bias_flag)))
+        layers.append(nn.Conv2d(512, 512, kernel_size=4, stride=1, padding=0))
+        #layers.append(nn.Sigmoid())
+
+        # all:
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = self.net(x)
+        y = y.mean()
+        return y # [1,1,1,1]
+
+
