dd7a1e5cf16b20c15acaa81db37384ed1a797920
disanda <skyanda1@qq.com>
2021-03-15 13:28:04 +0800

    celebaPG

--- networks/D2E_PG.py
+++ networks/D2E_PG.py
@@ -21,6 +21,7 @@ import torch.nn.utils.spectral_norm as spectral_norm
 import math
 import torch.nn.functional as F
 
+
 def get_parameter_number(net):
     total_num = sum(p.numel() for p in net.parameters())
     trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
--- train_one_dataSet.py
+++ train_one_dataSet.py
@@ -72,9 +72,9 @@ print('data-size:    '+str(shape))
 # ==============================================================================
 
 #G = net.Generator(input_dim=args.z_dim, output_channels = args.img_channels, image_size=args.img_size, scale=args.Gscale).to(device)
-G = net.Generator()
+G = net.Generator().to(device)
 print(G)
-D = net.Discriminator()
+D = net.Discriminator().to(device)
 #D = net.Discriminator_SpectrualNorm(input_dim=args.z_dim, input_channels = args.img_channels, image_size=args.img_size, Gscale=args.Gscale, Dscale=args.Dscale).to(device)
 print(D)
 x,y = net.get_parameter_number(G),net.get_parameter_number(D)
