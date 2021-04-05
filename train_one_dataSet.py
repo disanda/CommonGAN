3fe6a7f4fd4a50323aecc6319cb2a367bd9302aa
disanda <skyanda1@qq.com>
2021-04-05 15:50:07 +0800

    x

--- train_one_dataSet.py
+++ train_one_dataSet.py
@@ -32,8 +32,8 @@ parser.add_argument('--experiment_name', default='none')
 parser.add_argument('--img_size',type=int, default=512)
 parser.add_argument('--img_channels', type=int, default=3)# RGB:3 ,L:1
 parser.add_argument('--dataset', default='Celeba_HQ')#choices=['cifar10', 'fashion_mnist', 'mnist', 'celeba', 'anime', 'custom','Celeba_HQ'])
-parser.add_argument('--z_dim', type=int, default=256)
-parser.add_argument('--Gscale', type=int, default=8) # scale：网络隐藏层维度数,默认为 image_size//8 * image_size 
+parser.add_argument('--z_dim', type=int, default=512)
+parser.add_argument('--Gscale', type=int, default=4) # scale：网络隐藏层维度数,默认为 image_size//8 * image_size 
 parser.add_argument('--Dscale', type=int, default=1) 
 args = parser.parse_args()
 
