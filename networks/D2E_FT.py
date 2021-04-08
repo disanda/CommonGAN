471816a58aad7a3b0978aaba6243e118d590617c
disanda <skyanda1@qq.com>
2021-04-08 19:54:45 +0800

    x

---
+++ networks/D2E_FT.py
@@ -0,0 +1,92 @@
+#这一版调整了参数，最大参数为512，并调大了原先DCGAN中参数较小的层
+
+import torch
+from torch import nn
+import torch.nn.utils.spectral_norm as spectral_norm
+import math
+
+def get_parameter_number(net):
+    total_num = sum(p.numel() for p in net.parameters())
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return {'Total': total_num, 'Trainable': trainable_num}
+
+def get_para_GByte(parameter_number):
+     x=parameter_number['Total']*8/1024/1024/1024
+     y=parameter_number['Total']*8/1024/1024/1024
+     return {'Total_GB': x, 'Trainable_BG': y}
+
+class Generator(nn.Module):
+    def __init__(self, input_dim=512, output_channels=3, image_size=512, first_hidden_dim_ = 512, last_hidden_dim_ =64, another_times=0):
+        super().__init__()
+        layers = []
+        up_times = math.log(image_size,2)- 3 - another_times # 减去前两次 1->2->4， 及最后一次， 方便中间写循环
+        first_hidden_dim = first_hidden_dim_ # 这里对应输入维度，表示《输入维度》对应《网络中间层维度（起点）》的放大倍数
+        bias_flag = False
+
+        # 1: 
+        layers.append(nn.ConvTranspose2d(input_dim, first_hidden_dim, kernel_size=4,stride=1,padding=0,bias=bias_flag)) # 1*1 input -> 4*4
+        #layers.append(nn.ConvTranspose2d(input_dim, first_hidden_dim, kernel_size=4,stride=2,padding=1,bias=bias_flag)) # 4*4 input -> 8*8
+        layers.append(nn.BatchNorm2d(first_hidden_dim))
+        layers.append(nn.ReLU())
+
+        # 2: upsamplings, 4x4 -> 8x8 -> 16x16 -> 32*32
+        hidden_dim = first_hidden_dim
+        while up_times>0:
+            if up_times < (math.log(first_hidden_dim_,2) - math.log(last_hidden_dim_,2)+1):
+                layers.append(nn.ConvTranspose2d(hidden_dim, hidden_dim//2, kernel_size=4, stride=2, padding=1 ,bias=bias_flag))
+                layers.append(nn.BatchNorm2d(hidden_dim//2))
+                layers.append(nn.ReLU())
+                hidden_dim = hidden_dim // 2
+            else:
+                layers.append(nn.ConvTranspose2d(hidden_dim, hidden_dim, kernel_size=4, stride=2, padding=1 ,bias=bias_flag))
+                layers.append(nn.BatchNorm2d(hidden_dim))
+                layers.append(nn.ReLU())
+            up_times = up_times - 1
+
+        # 3:end 
+        layers.append(nn.ConvTranspose2d(hidden_dim,output_channels,kernel_size=4, stride=2, padding=1, bias=bias_flag))
+        layers.append(nn.Tanh())
+
+        # all
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, z):
+        x = self.net(z)
+        return x
+
+class Discriminator_SpectrualNorm(nn.Module):
+    def __init__(self, input_dim=512, input_channels=3, image_size=512, first_hidden_dim_=64, last_hidden_dim_=512, another_times=0): #新版的Dscale是相对G缩小的倍数
+        super().__init__()
+        layers=[]
+        up_times = math.log(image_size,2)- 3 - another_times
+        first_hidden_dim = first_hidden_dim_
+        bias_flag = False
+
+        # 1:
+        layers.append(spectral_norm(nn.Conv2d(input_channels, first_hidden_dim, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+        # 2: 64*64 > 4*4
+        hidden_dim = first_hidden_dim
+        while up_times>0:  
+            if hidden_dim < last_hidden_dim_:
+                layers.append(spectral_norm(nn.Conv2d(hidden_dim, hidden_dim*2, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
+                layers.append(nn.LeakyReLU(0.2, inplace=True))
+                hidden_dim = hidden_dim * 2
+            else:
+                layers.append(spectral_norm(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
+                layers.append(nn.LeakyReLU(0.2, inplace=True))
+            up_times = up_times - 1
+        # 3:
+        layers.append(nn.Conv2d(hidden_dim, input_dim, kernel_size=4, stride=1, padding=0)) # 4*4 > 1*1
+        #layers.append(nn.Conv2d(hidden_dim, input_dim, kernel_size=4, stride=2, padding=1)) # 8*8 > 4*4
+
+        # all:
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        y = self.net(x)
+        #y = y.mean()
+        return y # [1,1,1,1]
+
+
