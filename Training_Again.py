163be9844c368fe0438550ac91c4aef97ac74a30
disanda <skyanda1@qq.com>
2021-04-19 21:15:51 +0800

    x

--- Training_Again.py
+++ Training_Again.py
@@ -15,25 +15,21 @@ from torchsummary import summary
 import itertools
 import lpips
 
-# ==============================================================================
-# =                                   param                                    =
-# ==============================================================================
-
 # command line
 parser = argparse.ArgumentParser(description='the training args')
 parser.add_argument('--epochs', type=int, default=100)
 parser.add_argument('--lr', type=float, default=0.0002)
 parser.add_argument('--beta_1', type=float, default=0.5)
-parser.add_argument('--batch_size', type=int, default=30)
+parser.add_argument('--batch_size', type=int, default=60)
 parser.add_argument('--adversarial_loss_mode', default='gan', choices=['gan', 'hinge_v1', 'hinge_v2', 'lsgan', 'wgan'])
 parser.add_argument('--gradient_penalty_mode', default='none', choices=['none', '1-gp', '0-gp', 'lp'])
 parser.add_argument('--gradient_penalty_sample_mode', default='line', choices=['line', 'real', 'fake', 'dragan'])
 parser.add_argument('--gradient_penalty_weight', type=float, default=10.0)
 parser.add_argument('--experiment_name', default='none')
-parser.add_argument('--img_size',type=int, default=512)
+parser.add_argument('--img_size',type=int, default=256)
 parser.add_argument('--img_channels', type=int, default=3)# RGB:3 ,L:1
 parser.add_argument('--dataset', default='Celeba_HQ')#choices=['cifar10', 'fashion_mnist', 'mnist', 'celeba', 'anime', 'custom','Celeba_HQ'])
-parser.add_argument('--z_dim', type=int, default=512)
+parser.add_argument('--z_dim', type=int, default=256)
 parser.add_argument('--Gscale', type=int, default=8) # scale：网络隐藏层维度数,默认为 image_size//8 * image_size 
 parser.add_argument('--Dscale', type=int, default=1) 
 args = parser.parse_args()
@@ -45,7 +41,7 @@ if args.experiment_name == 'none':
     if args.gradient_penalty_mode != 'none':
         args.experiment_name += '_%s_%s' % (args.gradient_penalty_mode, args.gradient_penalty_sample_mode)
 
-args.experiment_name += '_Gs%d_Ds%d_Zdim%d_imgSize%d_batch_size%d_512pixel_512dim' % (args.Gscale, args.Dscale, args.z_dim, args.img_size,args.batch_size)
+args.experiment_name += '_Gs%d_Ds%d_Zdim%d_imgSize%d_batch_size%d_256pixel_reBest_G(IN)' % (args.Gscale, args.Dscale, args.z_dim, args.img_size,args.batch_size)
 
 output_dir = os.path.join('output', args.experiment_name)
 
@@ -54,11 +50,8 @@ if not os.path.exists('output'):
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-# save settings
-
 with open(os.path.join(output_dir, 'settings.yml'), "w", encoding="utf-8") as f:
     yaml.dump(args, f)
-
 
 # others
 use_gpu = torch.cuda.is_available()
@@ -66,54 +59,21 @@ device = torch.device("cuda" if use_gpu else "cpu")
 
 # dataset
 data_loader, shape = data.make_dataset(args.dataset, args.batch_size, args.img_size,pin_memory=use_gpu)
-#n_G_upsamplings = n_D_downsamplings = 5 # 3: 32x32  4:64:64 5:128 6:256
 print('data-size:    '+str(shape))
 
-# ==============================================================================
-# =                                   model                                    =
-# ==============================================================================
 G = net.Generator(input_dim=args.z_dim, output_channels = args.img_channels, image_size=args.img_size, scale=args.Gscale, another_times=another_times_).to(device)
 D = net.Discriminator_SpectrualNorm(input_dim=args.z_dim, input_channels = args.img_channels, image_size=args.img_size, Gscale=args.Gscale, Dscale=args.Dscale, another_times=another_times_).to(device)
-#G = net.Generator_v3().to(device)
-#D = net.Discriminator_SpectrualNorm_v3().to(device)
-#G.load_state_dict(torch.load('./pre-model/G_in256_G8.pth',map_location=device)) #shadow的效果要好一些 
-#D.load_state_dict(torch.load('./pre-model/D_in256_D4.pth',map_location=device))
 summary(G,(args.z_dim,1,1))
 summary(D,(3,args.img_size,args.img_size))
-x,y = net.get_parameter_number(G),net.get_parameter_number(D)
-x_GB, y_GB = net.get_para_GByte(x),net.get_para_GByte(y)
-
-
-with open(output_dir+'/net.txt','w+') as f:
-    #if os.path.getsize(output_dir+'/net.txt') == 0: #判断文件是否为空
-        print(G,file=f)
-        print(x,file=f)
-        print(x_GB,file=f)
-        print('-------------------',file=f)
-        print(D,file=f)
-        print(y,file=f)
-        print(y_GB,file=f)
-
-# adversarial_loss_functions
-d_loss_fn, g_loss_fn = loss_func.get_adversarial_losses_fn(args.adversarial_loss_mode)
-
 
 # optimizer
 G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
 D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
-#D2E_optimizer = torch.optim.Adam(itertools.chain(G.parameters(), D.parameters()),lr=0.0001,betas=(0.6, 0.95),amsgrad=True)#G,D都更新
-#decayG = torch.optim.lr_scheduler.ExponentialLR(G_optimizer, gamma=1)
-#decayD = torch.optim.lr_scheduler.ExponentialLR(D_optimizer, gamma=1)
-
 
 @torch.no_grad()
 def sample(z):
     G.eval()
     return G(z)
-
-# ==============================================================================
-# =                                    run                                     =
-# ==============================================================================
 
 if __name__ == '__main__':
     ckpt_dir = os.path.join(output_dir, 'checkpoints')
@@ -195,7 +155,6 @@ if __name__ == '__main__':
             #     #print(l2)
             #     #print(l3)
 
-
             # GE_loss_dict = {'gD_loss': DE_loss}
             # for k, v in GE_loss_dict.items():
             #     writer.add_scalar('GD/%s' % k, v.data.cpu().numpy(), global_step=it_g)
@@ -207,31 +166,7 @@ if __name__ == '__main__':
                     with open(output_dir+'/loss.txt','a+') as f:
                         print('G_loss:'+str(G_loss)+'------'+'D_loss'+str(D_loss),file=f)
                         print('------------------------')
-
-                #G
-                z = torch.randn(1,512,1,1)
-                for name, layer in G.net._modules.items():
-                    z = layer(z)
-                    if isinstance(layer, torch.nn.ConvTranspose2d):
-                        print(z.shape)
-                        x1 = z.transpose(0, 1)  # C，B, H, W  ---> B，C, H, W
-                        img_grid = torchvision.utils.make_grid(x1, normalize=True, scale_each=True, nrow=30)  # B，C, H, W
-                        writer.add_image('feature_maps_G', img_grid)
-                        #torchvision.utils.save_image(x1,'feature_maps%s.png'%name, nrow=100)
-
-                #D
-                x = z
-                for name, layer in D.net._modules.items():
-                   x = layer(x)
-                   if isinstance(layer, torch.nn.Conv2d):
-                       x1 = x.transpose(0, 1)  # C，B, H, W  ---> B，C, H, W
-                       img_grid = torchvision.utils.make_grid(x1, normalize=True, scale_each=True, nrow=30)  # B，C, H, W
-                       writer.add_image('feature_maps_D', img_grid)
-                       #torchvision.utils.save_image(x1,'./D_feature_maps%s.png'%name, nrow=20)
-
         # save checkpoint
         if (ep+1)%10==0:   
-            #torch.save(G.state_dict(), ckpt_dir+'/Epoch_G.pth') #保存每次需要覆盖
-            #torch.save(D.state_dict(), ckpt_dir+'/Epoch_D.pth')
             torch.save(G.state_dict(), ckpt_dir+'/Epoch_G_%d.pth' % ep)
             torch.save(D.state_dict(), ckpt_dir+'/Epoch_D_%d.pth' % ep)
